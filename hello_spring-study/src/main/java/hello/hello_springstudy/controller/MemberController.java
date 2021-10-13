b73bf94affcd8839369dde44b030c36de0199a25
2theMoon <dlaguddh1@gmail.com>
2021-10-13 17:11:32 +0900

    Connect H2 with jdbcDriver

--- hello_spring-study/src/main/java/hello/hello_springstudy/controller/MemberController.java
+++ hello_spring-study/src/main/java/hello/hello_springstudy/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_springstudy.controller;
 
+import hello.hello_springstudy.domain.Member;
 import hello.hello_springstudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -16,4 +22,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new") //링크 이동시
+    public String createForm() {
+        return "members/createMemberForm";  // 폼 html을 뿌려주고
+    }
+
+    @PostMapping("/members/new") // 폼 html에서 받은 name값을 멤버 서비스 조인 메서드를 이용해 레포지토리 저장
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members") // Model : request.setArrtibute 와 비슷 한 역할
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
---
+++ hello_spring-study/src/main/java/hello/hello_springstudy/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.hello_springstudy.repository;
+
+import hello.hello_springstudy.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
